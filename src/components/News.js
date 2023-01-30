13e78b3197337ea65e3ab7b3293eef4ec92c3c85
Yuvraj <yuvrajdholia@gmail.com>
2023-01-30 08:45:41 +0530

    Added infinite scroll

--- src/components/News.js
+++ src/components/News.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 import Spinner from './Spinner';
 import PropTypes from 'prop-types'
+import InfiniteScroll from "react-infinite-scroll-component";
 
 
 export class News extends Component {
@@ -28,7 +29,8 @@ export class News extends Component {
     this.state = {
       articles : [],
       loading : false,
-      page : 1
+      page : 1,
+      totalResults : 0
     }
       document.title = `${this.capitalizeFirstLetter(this.props.category)} - NewsMonkey`;
   }
@@ -96,7 +98,24 @@ export class News extends Component {
   // }
     this.setState({page : this.state.page - 1});
     this.updateNews();
+    
   }
+
+  fetchMoreData = async () => {
+    this.setState({page : this.state.page + 1});
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=dd156d85ae5f4eafac1c1821fe6906f4&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    this.setState({loading: true});
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData);
+    this.setState({
+      // articles: parsedData.articles, 
+      //Now I am using concatenation here
+      articles: this.state.articles.concat(parsedData.articles),
+      totalResults: parsedData.totalResults,
+      loading : false
+    });
+  };
 
 
 
@@ -107,24 +126,40 @@ export class News extends Component {
     
 
     return (
-      <div className='container my-3'>
+      <>
         <h1 className="text-center" style={{margin : '20px 0'}}> NewsClub- Top {this.capitalizeFirstLetter(this.props.category)} Headlines </h1>
-        {this.state.loading && <Spinner />}
+        {/* {this.state.loading && <Spinner />} */}
         
-        <div className="row">
-        {!this.state.loading && this.state.articles.map((element)=>{
+        <InfiniteScroll
+          dataLength={this.state.articles.length}
+          next={this.fetchMoreData}
+          hasMore={this.state.articles.length !== this.state.totalResults}
+          loader={<Spinner />}
+        >
+          <div className="container">
+
+          <div className="row">
+          {this.state.articles.map((element)=>{
           return <div className="col-md-4" key={element.url}>
             <NewsItem title= {element.title?element.title:""} description={element.description?element.description:""} author={element.author} date={element.publishedAt} source={element.source.name} imageUrl = {element.urlToImage} newsUrl = {element.url}/>
           </div>})}
           
         </div>
-        <div className="container d-flex justify-content-between">
+
+
+          </div>
+        
+        </InfiniteScroll>
+
+
+          {/* Now we are using infinite scroll */}
+        {/* <div className="container d-flex justify-content-between">
           <button disabled={this.state.page<=1} type="button" className="btn btn-dark" onClick={this.handlePreviousClick}>&larr; Previous</button>
           <button disabled={this.state.page+1 > Math.ceil(this.state.totalResults/this.props.pageSize)} type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
-        </div>
+        </div> */}
         
         
-      </div>
+      </>
     )
   }
 }
